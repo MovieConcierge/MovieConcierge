0062a71e55301621d82dda289b7b1a00670831dd
DESKTOP-SCOGBPT\hatim <hatimfertat2001@gmail.com>
2023-12-22 00:00:53 +0900

    implemented the api to get the movie title, info and poster to show up randomly from the 50 current most popular movies

--- Class-project/Assets/Scripts/MovieDisplay.cs
+++ Class-project/Assets/Scripts/MovieDisplay.cs
@@ -10,7 +10,7 @@ public class MovieDisplay : MonoBehaviour
     public GameObject moviePoster;
     public GameObject movieInfo;
     public static string newTitle;
-    public static Sprite newPoster;
+    public static Texture2D newPosterTexture;
     public static string newInfo;
 
     void Start()
@@ -23,13 +23,52 @@ public class MovieDisplay : MonoBehaviour
     {
         yield return new WaitForSeconds(0.25f);
 
+        //Debug.Log("new poster dimensions: " + newPoster.rect.width + "x" + newPoster.rect.height);
+
         DisplayMovie();
     }
 
     void DisplayMovie()
     {
         movieTitle.GetComponent<TextMeshProUGUI>().text = newTitle;
-        moviePoster.GetComponent<Image>().sprite = newPoster;
+        moviePoster.GetComponent<Image>().sprite = CreateSpriteFromTexture(newPosterTexture, moviePoster.GetComponent<RectTransform>());
+        
         movieInfo.GetComponent<TextMeshProUGUI>().text = newInfo;
+    }
+
+    Sprite CreateSpriteFromTexture(Texture2D texture, RectTransform rectTransform)
+    {
+        // Retrieve target dimensions from RectTransform
+        int targetWidth = Mathf.RoundToInt(rectTransform.rect.width);
+        int targetHeight = Mathf.RoundToInt(rectTransform.rect.height);
+
+        // Resize the texture to the target dimensions
+        Texture2D resizedTexture = ResizeTexture(texture, targetWidth, targetHeight);
+
+        // Create a sprite from the resized texture
+        Sprite sprite = Sprite.Create(
+            resizedTexture,
+            new Rect(0, 0, resizedTexture.width, resizedTexture.height),
+            new Vector2(0.5f, 0.5f));
+
+        return sprite;
+    }
+
+    Texture2D ResizeTexture(Texture2D sourceTexture, int targetWidth, int targetHeight)
+    {
+        RenderTexture rt = RenderTexture.GetTemporary(targetWidth, targetHeight);
+        rt.filterMode = FilterMode.Bilinear;
+        RenderTexture.active = rt;
+
+        Graphics.Blit(sourceTexture, rt);
+
+        Texture2D resizedTexture = new Texture2D(targetWidth, targetHeight);
+        resizedTexture.ReadPixels(new Rect(0, 0, targetWidth, targetHeight), 0, 0);
+        resizedTexture.Apply();
+
+        RenderTexture.active = null;
+        RenderTexture.ReleaseTemporary(rt);
+
+        return resizedTexture;
     }
 }
